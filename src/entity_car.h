388a647f9028ac6ff8d4687feb446f96caec8f88
Tubbles <jae91m@gmail.com>
2026-03-01 14:35:07 +0100

    Narrow car collision width to align with body panels
    CAR_HALF_W 22 -> 17, excluding side mirrors from the collision shape.

--- src/entity_car.h
+++ src/entity_car.h
@@ -4,7 +4,7 @@
 #include "entity.h"
 
 #define CAR_SIZE 100.0f
-#define CAR_HALF_W 22.0f
+#define CAR_HALF_W 17.0f
 #define CAR_HALF_H 43.0f
 
 Entity entity_car_init(int gamepad_id, Vector2 pos, Color color, Texture2D *tex);
